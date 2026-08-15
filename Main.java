b05f61b5dfad6657e1e191ff107a71adfdff4cda
Mohammad A Ahmad <khadimnyc@gmail.com>
2026-08-15 17:48:05 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+import processing.core.PApplet;
+
+public class Main {
+	public static void main(String[] args) {
+        // Initialize the Processing app.
+		String[] appletArgs = new String[] { "Sketch" };
+		PApplet.main(appletArgs);
+    }
+}
